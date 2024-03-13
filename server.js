90a8f8abb5419a0dfc98b46b52d43c2672192142
Jonathan Funk <jon.m.funk@gmail.com>
2024-03-13 13:56:32 -0700

    connected mongoose

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT || 8000;
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 
